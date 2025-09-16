8b468c8bb2d7dcfb59403944034da53a3581b66b
FHAUKEM <14070548+FHAUKEM@users.noreply.github.com>
2025-09-16 13:33:10 +0200

    DSP: fix progression rules

--- worlds/dsp/Rules.py
+++ worlds/dsp/Rules.py
@@ -11,10 +11,10 @@ def set_rules(dsp_world: "DSPWorld"):
 
     # Region rules
     set_rule(multiworld.get_entrance("Menu -> Game Start", player), lambda state: True)
-    set_rule(multiworld.get_entrance("Game Start -> Electromagnetic Matrix", player), lambda state: True)
-    set_rule(multiworld.get_entrance("Electromagnetic Matrix -> Energy Matrix", player), lambda state: state.has("Electromagnetic Matrix", player))
-    set_rule(multiworld.get_entrance("Energy Matrix -> Structure Matrix", player), lambda state: state.has("Energy Matrix", player))
-    set_rule(multiworld.get_entrance("Structure Matrix -> Information Matrix", player), lambda state: state.has("Structure Matrix", player))
-    set_rule(multiworld.get_entrance("Information Matrix -> Gravity Matrix", player), lambda state: state.has("Information Matrix", player))
-    set_rule(multiworld.get_entrance("Gravity Matrix -> Universe Matrix", player), lambda state: state.has("Gravity Matrix", player))
+    set_rule(multiworld.get_entrance("Game Start -> Electromagnetic Matrix", player), lambda state: state.has("Electromagnetic Matrix", player))
+    set_rule(multiworld.get_entrance("Electromagnetic Matrix -> Energy Matrix", player), lambda state: state.has("Energy Matrix", player))
+    set_rule(multiworld.get_entrance("Energy Matrix -> Structure Matrix", player), lambda state: state.has("Structure Matrix", player))
+    set_rule(multiworld.get_entrance("Structure Matrix -> Information Matrix", player), lambda state: state.has("Information Matrix", player))
+    set_rule(multiworld.get_entrance("Information Matrix -> Gravity Matrix", player), lambda state: state.has("Gravity Matrix", player))
+    set_rule(multiworld.get_entrance("Gravity Matrix -> Universe Matrix", player), lambda state: state.has("Universe Matrix", player))
     set_rule(multiworld.get_entrance("Universe Matrix -> Goal Complete", player), lambda state: state.has("Universe Matrix", player))
